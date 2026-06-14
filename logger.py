6e7d197cf6afb53a18b132a80dd21a0471b60922
aristapanell-cell <aristapanell@gmail.com>
2026-06-14 10:23:33 +0330

    Update logger.py

--- logger.py
+++ logger.py
@@ -15,6 +15,11 @@ file_formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(file_formatter)
+logger.addHandler(console_handler)
+
 stats = {
     "releases_sent": 0,
     "total_size_bytes": 0,
